8318475eeb96b323b9d7e82479a595627491745b
jae kim <jaehkim23@gmail.com>
2023-11-08 13:18:06 -0600

    fixed signupform

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,22 +2,22 @@ const router = require('express').Router();
 // import models here
 const { User, Workout } = require('../../models');
 
-//import auth?
+
 
 // CREATE new user
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
+      name:req.body.name,
+      email:req.body.email,
+      password:req.body.password,
     });
 
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.loggedIn = true;
-
-      res.status(200).json(userData);
+      res.status(200).json({user: userData});
+   
     });
   } catch (err) {
     console.log(err);
--- public/js/login.js
+++ public/js/login.js
@@ -24,25 +24,28 @@ const loginFormHandler = async (event) => {
 
 //Signup function on Login/Signup page - need to add correct fetch route 
 const signupFormHandler = async (event) => {
-    event.prevant.defualt();
+    event.preventDefault();
 
     //Get full name/email/password from signup form 
-    const fullName = document.querySelector('#name-signup')
-    const email = document.querySelector('#email-signup')
-    const password = document.querySelector('#password-signup')
+    const name = document.querySelector('#name-signup').value;
+    const email = document.querySelector('#email-signup').value;
+    const password = document.querySelector('#password-signup').value;
+    console.log(email);
+    console.log(password);
+    console.log(name)
 //if full name/email/password are filled out 
-    if (fullName && email && password){
+    if (name && email && password){
         const response = await fetch('/api/users', {
             method:'POST',
-            body:JSON.stringify({fullName, email, password}),
+            body:JSON.stringify({name, email, password}),
             headers: { 'Content-Type': 'application/json' },
-        })
-
+        });
+        console.log(response);
         if (response.ok){
-            document.location.replace('/userpage')
+            document.location.replace('/profile');
         }
     }
-}
+};
 
-document.querySelector('#mainLogIn').addEventListener('submit', loginFormHandler)
-document.querySelector('#signup').addEventListener('submit', signupFormHandler)
+document.querySelector('#mainLogIn').addEventListener('submit', loginFormHandler);
+document.querySelector('#signup').addEventListener('submit', signupFormHandler);
